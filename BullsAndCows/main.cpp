1cdac091e659e6be40cad58bfb5524cbd4a699b8
Daniel Park <dpark189@gmail.com>
2018-10-02 16:29:41 -0400

    Initial Commit

---
+++ BullsAndCows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullsAndCows
+//
+//  Created by asdf on 10/2/18.
+//  Copyright © 2018 asdf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
